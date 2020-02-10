b314c9140beec5c2f20fd77382b9484d8ceb9aff
Marcos Mercedes <marcos.mercedesn@gmail.com>
2020-02-10 11:46:03 -0400

    Add compiled docs

---
+++ docs/precache-manifest.901b0b21be565455aeb5d6df7ca72e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6230a92df2d89e29693ad50fbfff50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "848581a95e3fcedd6fcf",
+    "url": "/static/css/main.af3ea7b2.chunk.css"
+  },
+  {
+    "revision": "26f4b163851a69132112",
+    "url": "/static/js/2.ddddf0e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ddddf0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848581a95e3fcedd6fcf",
+    "url": "/static/js/main.ccb084ef.chunk.js"
+  },
+  {
+    "revision": "30f2c15a9e61a4cee959",
+    "url": "/static/js/runtime-main.b3349478.js"
+  }
+]);
