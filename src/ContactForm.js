49074574c008b5e661b7dfb674d6084e43f4e322
Marcos Mercedes <marcos.mercedesn@gmail.com>
2020-02-10 11:37:11 -0400

    Create ContactForm.js

---
+++ src/ContactForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ContactForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <div className="form-group">
+          <input type="text" className="form-control" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
